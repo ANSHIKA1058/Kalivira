5d1cc74b0483d0cb7d5cb3cbf6bb06531e353124
Anshika <puniaanshika456@gmail.com>
2026-07-25 22:18:11 +0530

    Implemeneted basic REST API setup with HelloController

--- backend/src/main/java/com/kalivira/controller/HelloController.java
+++ backend/src/main/java/com/kalivira/controller/HelloController.java
@@ -1,10 +1,24 @@
 package com.kalivira.controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
+@RequestMapping("/api")
 public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "Welcome to Kalivira API";
     }
+    @PostMapping("/upload")
+    public String upload(){
+        return "File Uploaded Successfully";
+    }
+    @PutMapping("/update")
+    public String update(){
+        return "File Updated Successfully";
+    }
+    @DeleteMapping("/delete")
+    public String delete(){
+        return "File Deleted Successully";
+    }
+
 }
